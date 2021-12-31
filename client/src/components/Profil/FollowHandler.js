c7ffd9e327d1c2876e79550c5d2b96d44c304267
LaonaGruenais <emmie.laona@gmail.com>
2021-12-31 12:06:17 +0100

    fil d'actualité / err de connexion

--- client/src/components/Profil/FollowHandler.js
+++ client/src/components/Profil/FollowHandler.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { followUser, unfollowUser } from '../../actions/user.actions';
 import { isEmpty } from '../Utils';
 
-const FollowHandler = ({ idToFollow }) => {
+const FollowHandler = ({ idToFollow, type }) => {
     const userData = useSelector((state) => state.userReducer)
     const [isFollowed, setIsFollowed] = useState(false)
     const dispatch = useDispatch();
@@ -15,7 +15,7 @@ const FollowHandler = ({ idToFollow }) => {
     }
 
     // Ne plus suivre une personne qui nous suis directement dans la liste des abonnés
-    const handleUnfollow = () => {
+    const handleUnfollow = ({ idToFollow, type }) => {
         dispatch(unfollowUser(userData._id, idToFollow));
         setIsFollowed(false);
     }
@@ -32,10 +32,14 @@ const FollowHandler = ({ idToFollow }) => {
         <>
             {isFollowed && !isEmpty(userData) && (
                 <span onClick={handleUnfollow}>
+                    {type === 'suggestion' && <button className='unfollow-btn'>Abonné</button>}
+                    {type === 'card' && <img src='./img/icons/checked.svg' alt='checked' />}
                 </span>
             )}
             {isFollowed === false && !isEmpty(userData) && (
                 <span onClick={handleFollow}>
+                    {type === 'suggestion' && <button className='follow-btn'>Suivre</button>}
+                    {type === 'card' && <img src='./img/icons/check.svg' alt='check' />}
                 </span>
             )}
         </>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UidContext } from '../components/AppContext';
+import LeftNav from '../components/LeftNav';
+// import NewPostForm from '../components/Post/NewPostForm';
+import Thread from '../components/Thread';
+import Log from '../components/Log';
+// import Trends from "../components/Trends";
+// import FriendsHint from "../components/Profil/FriendsHint";
 
 const Home = () => {
+  const uid = useContext(UidContext);
+
   return (
-    <div>
-        Bienvenue sur la page d'accueil
+    <div className='home'>
+      <LeftNav />
+      <div className='main'>
+        <Thread />
+      </div>
     </div>
   )
 }
